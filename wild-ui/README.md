75f76db6fde609340abfe2d6d4ef9cb6c4a90321
aaron1a12 <aaron.escobar@wildmontage.com>
2024-04-25 13:31:59 -0400

    UI

---
+++ wild-ui/README.md
@@ -0,0 +1,2 @@
+# wild-ui
+To avoid creating excessive iframes, which would undoubtedly increase memory usage, a single NUI is used.
---
+++ wild-ui/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+games {"rdr3"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+client_script {
+	"@wild-core/client/functions/cl_dataview.lua",
+	"@wild-core/client/functions/cl_general.lua",
+	'client/cl_ui.lua'
+}
+
+server_script {
+}
+
+dependencies {
+    'wild-core'
+}
