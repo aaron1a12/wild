036f7c633cff9f07ffb0b15155de87d662530e23
aaron1a12 <aaron.escobar@wildmontage.com>
2024-04-29 12:09:10 -0400

    Reworked loot script to use new event system

--- wild-core/client/cl_loot.lua
+++ wild-core/client/cl_loot.lua
@@ -1,43 +1,19 @@
-local function RegisterDecorTypes()
-	DecorRegister("ped_looted", 2);
+-- TODO: this could be better
+local function CalculateLootForPed(ped)
+	math.randomseed(GetGameTimer()/7)
+	local money = math.random(1, 50) / 100
+
+	return money
 end
-RegisterDecorTypes()
 
-local lastLootedPed = 0
+W.Events.AddHandler(`EVENT_LOOT_COMPLETE`, function(data)
+	local playerPed = GetPlayerPed(player)
 
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(100)
-	
-	        local playerPed = GetPlayerPed(player)
-	
-	        if GetPedLootStatusMp(playerPed) == 1 then -- Attempting to loot
-	
-	            local lootTarget = GetClosestPedTo(playerPed, 1.0)
-	
-	            if lootTarget ~= lastLootedPed then
-	                Citizen.CreateThread(function()
-	                    local timeOut = 0
-	    
-	                    while (lootTarget ~= lastLootedPed) and timeOut < 2.0 do
-	                        Citizen.Wait(0)
-	                        timeOut = timeOut + GetFrameTime()
-	    
-	                        if IsEntityFullyLooted(lootTarget) == 1 and lootTarget ~= lastLootedPed then -- Done looting
-	                            lastLootedPed = lootTarget
-	                            DecorSetBool(lootTarget, "ped_looted", true)
-	                            timeOut = 999.0
-	                            
-	                            Citizen.Wait(500)
-	
-	                            math.randomseed(GetGameTimer()/7)
-	                            local money = math.random(1, 50) / 100
+	local looterPed = data[1]
+	local ped = data[2]
+	local success = data[3]
 
-	                            TriggerServerEvent("wild:sv_giveMoney", GetPlayerName(PlayerId()), money)
-	                        end
-	                    end
-	                end)
-	            end          
-	        end
+	if looterPed == playerPed and success == 1 then
+		TriggerServerEvent("wild:sv_giveMoney", GetPlayerName(PlayerId()), CalculateLootForPed(ped))
 	end
 end)
