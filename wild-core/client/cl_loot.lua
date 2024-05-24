dcede6c332ca6a6b0d29ff5cab849401e8cc13fc
aaron1a12 <aaron.escobar@wildmontage.com>
2024-05-24 19:27:07 -0400

    Check for witness to remove honor

--- wild-core/client/cl_loot.lua
+++ wild-core/client/cl_loot.lua
@@ -16,6 +16,19 @@ W.Events.AddHandler(`EVENT_LOOT_COMPLETE`, function(data)
 	if looterPed == playerPed and success == 1 then
 		if GetMetaPedType(ped) ~= 3 then -- animal = 3
 			TriggerServerEvent("wild:sv_giveMoney", GetPlayerName(PlayerId()), CalculateLootForPed(ped))
+
+			-- Check for witnesses
+			local playerCoords = GetEntityCoords(PlayerPedId())
+
+			for _, witnessPed in ipairs(GetGamePool('CPed')) do
+				if GetVectorDistSqr(GetEntityCoords(witnessPed), playerCoords) < 300.0 then
+					if not IsPedAPlayer(witnessPed) and IsPedHuman(witnessPed) then
+						if IsTargetPedInPerceptionArea(witnessPed, playerPed, -1.0, -1.0, -1.0, -1.0) then -- Player is totally within sight
+							W.AddPlayerHonor(W.Config.Honor["onLootWitnessed"])
+						end
+					end
+				end
+			end
 		end
 	end
 end)
